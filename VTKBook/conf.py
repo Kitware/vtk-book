f9b5e77cf68143e65cf73f8298b29da5f907aaf1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-05-25 19:52:55 -0400

    Simplify layout removing "VTKBook" from the URL
    This commit updates the Makefile/make.bat to expect the sources to
    be in the "VTKBook" directory.

--- VTKBook/conf.py
+++ VTKBook/conf.py
@@ -8,9 +8,9 @@
 
 from datetime import date
 
-project = 'VTK Book'
-copyright = f'1993-{date.today().year}, VTK Book Authors and Contributors'
-author = 'VTK Book Authors and Contributors'
+project = "VTK Book"
+copyright = f"1993-{date.today().year}, VTK Book Authors and Contributors"
+author = "VTK Book Authors and Contributors"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -22,23 +22,19 @@ extensions = [
     "myst_parser",
 ]
 
-myst_enable_extensions = [ 
+myst_enable_extensions = [
     "amsmath",
     "dollarmath", # support latex equation inside $ $
     "linkify", # convert bare links to hyperlinks
 ]
 myst_heading_anchors = 7
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = [
-    "_build",
     "Thumbs.db",
     ".DS_Store",
-    "README.md",
-    "VTKBook/Figures/README.md",
+    "Figures/README.md",
 ]
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -49,4 +45,4 @@ html_theme_options = {
     "use_repository_button": True,
 }
 html_title = "VTK Book"
-html_static_path = ['_static']
+html_static_path = ["_static"]
--- VTKBook/index.md
+++ VTKBook/index.md
@@ -7,8 +7,20 @@ resources for the fourth edition of the  [VTK textbook](https://vtk.org/vtk-text
 ```{toctree}
 :maxdepth: 1
 :caption: Contents
-:glob:
 about_page
 citing
-VTKBook/*
+00Preface.md
+01Chapter1.md
+02Chapter2.md
+03Chapter3.md
+04Chapter4.md
+05Chapter5.md
+06Chapter6.md
+07Chapter7.md
+08Chapter8.md
+09Chapter9.md
+10Chapter10.md
+11Chapter11.md
+12Chapter12.md
+13Glossary.md
 ```
