428d3c9c0b28adc79da40bd4a9771710ea3c42ff
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-05-28 23:34:02 -0400

    conf: Fix formatting
    * Use double quotes instead of single quotes
      See https://black.readthedocs.io/en/stable/the_black_code_style/current_style.html#strings
    
    * Use 2 spaces before comment
      See https://black.readthedocs.io/en/stable/the_black_code_style/current_style.html#comments

--- conf.py
+++ conf.py
@@ -8,28 +8,28 @@
 
 from datetime import date
 
-project = 'VTK Book'
-copyright = f'1993-{date.today().year}, VTK Book Authors and Contributors'
-author = 'VTK Book Authors and Contributors'
+project = "VTK Book"
+copyright = f"1993-{date.today().year}, VTK Book Authors and Contributors"
+author = "VTK Book Authors and Contributors"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
     "myst_parser",
 ]
 
-myst_enable_extensions = [ 
+myst_enable_extensions = [
     "amsmath",
-    "dollarmath", # support latex equation inside $ $
-    "linkify", # convert bare links to hyperlinks
+    "dollarmath",  # support latex equation inside $ $
+    "linkify",  # convert bare links to hyperlinks
 ]
 myst_heading_anchors = 7
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = [
     "_build",
     "Thumbs.db",
@@ -37,8 +37,6 @@ exclude_patterns = [
     "README.md",
     "VTKBook/Figures/README.md",
 ]
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -49,4 +47,4 @@ html_theme_options = {
     "use_repository_button": True,
 }
 html_title = "VTK Book"
-html_static_path = ['_static']
+html_static_path = ["_static"]
